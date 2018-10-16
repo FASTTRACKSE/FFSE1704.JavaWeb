130eb340b2ec3648c41e3e2dee024e3ea0ecb576
khanhFFSE1701011 <32625914+khanhFFSE1701011@users.noreply.github.com>
2018-10-16 14:24:16 +0700

    Up

---
+++ KhanhCN/projectJSF/src/model/form/Upload_File.java
@@ -0,0 +1,80 @@
+package model.form;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Part;
+/**
+ *
+ * @author ntdan
+ */
+@ManagedBean
+@RequestScoped
+public class Upload_File {
+ 
+    private Part file;
+    private String fileName;
+    private long fileSize;
+     
+    /**
+     * Creates a new instance of Upload_File
+     */
+    public Upload_File() {
+    }
+ 
+    public Part getFile() {
+        return file;
+    }
+ 
+    public void setFile(Part file) {
+        this.file = file;
+    }
+ 
+    public String getFileName() {
+        return fileName;
+    }
+ 
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+     
+    public String upload()
+    {
+        try {
+            // get name of selected file
+            fileName = file.getSubmittedFileName();
+            // get file's size
+            fileSize = file.getSize();
+            // get fullpath of opload folder in web root
+            String dirPath= FacesContext.getCurrentInstance().getExternalContext().getRealPath("/upload");
+            // write file to upload folder
+            File path = new File(dirPath);
+
+            if (!path.exists()) {
+                path.mkdirs();
+            }
+
+
+            file.write(dirPath + "/" + fileName);
+             
+        } catch (IOException ex) {
+            Logger.getLogger(Upload_File.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+        return "viewUpload";
+    }
+ 
+    public long getFileSize() {
+        return fileSize;
+    }
+ 
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+}
