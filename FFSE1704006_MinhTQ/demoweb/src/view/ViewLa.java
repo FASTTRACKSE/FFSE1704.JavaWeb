abd85bdf6c276c10d9854445e1735df70ae1f1d7
ffse1704006 <36950238+ffse1704006@users.noreply.github.com>
2018-10-23 14:06:29 +0700

    up bai

--- FFSE1704006_MinhTQ/demoweb/src/view/ViewLa.java
+++ FFSE1704006_MinhTQ/demoweb/src/view/ViewLa.java
@@ -9,31 +9,33 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ViewLa
  */
-public class ViewLa extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class ViewLa extends HttpServlet { 
+	
        
+    
     /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ViewLa() {
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	public ViewLa() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+		String username =request.getParameter("username");
+		String password = request.getParameter("password");
+		
 	}
 
 }
