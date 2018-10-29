39b3e1fbafbbe90a5e16fa0c54951681f8d52441
truongdainghia <36950369+truongdainghia@users.noreply.github.com>
2018-10-29 12:18:13 +0700

    update

--- NGHIA TD/DISampleHocSinh/src/quanlihocsinh/entity/HeDaoTaoDaiHan.java
+++ NGHIA TD/DISampleHocSinh/src/quanlihocsinh/entity/HeDaoTaoDaiHan.java
@@ -1,12 +1,5 @@
 package quanlihocsinh.entity;
 
-<<<<<<< HEAD
-public class HeDaoTaoDaiHan implements HeDaoTao {
-	
-	String maHeDaoTao= "MA0127";
-	String thongTinHeDaoTao="Đào tạo dài hạn 16 tháng, full các môn lập trình";
-	MonHoc monHoc;
-=======
 import java.util.List;
 
 import quanlihocsinh.monhoc.MonHoc;
@@ -14,51 +7,10 @@ import quanlihocsinh.monhoc.MonHoc;
 public class HeDaoTaoDaiHan implements HeDaoTao {
 	List<MonHoc> dsMonHoc;
 	
->>>>>>> c815716f8d2212d969eef3865790586a577fadc5
 	public HeDaoTaoDaiHan() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-<<<<<<< HEAD
-	public HeDaoTaoDaiHan(String maHeDaoTao, String thongTinHeDaoTao, MonHoc monHoc) {
-		super();
-		this.maHeDaoTao = maHeDaoTao;
-		this.thongTinHeDaoTao = thongTinHeDaoTao;
-		this.monHoc = monHoc;
-	}
-	public String getMaHeDaoTao() {
-		return maHeDaoTao;
-	}
-	public void setMaHeDaoTao(String maHeDaoTao) {
-		this.maHeDaoTao = maHeDaoTao;
-	}
-	public String getThongTinHeDaoTao() {
-		return thongTinHeDaoTao;
-	}
-	public void setThongTinHeDaoTao(String thongTinHeDaoTao) {
-		this.thongTinHeDaoTao = thongTinHeDaoTao;
-	}
-	public MonHoc getMonHoc() {
-		return monHoc;
-	}
-	public void setMonHoc(MonHoc monHoc) {
-		this.monHoc = monHoc;
-	}
-=======
-
-	@Override
-	public String getMaHeDaoTao() {
-		// TODO Auto-generated method stub
-		return "ê3";
-	}
-
-	@Override
-	public String getThongTinHeDaoTao() {
-		// TODO Auto-generated method stub
-		return "Dai hạn";
-	}
-
-
 	public List<MonHoc> getDsMonHoc() {
 		return dsMonHoc;
 	}
@@ -66,13 +18,21 @@ public class HeDaoTaoDaiHan implements HeDaoTao {
 	public void setDsMonHoc(List<MonHoc> dsMonHoc) {
 		this.dsMonHoc = dsMonHoc;
 	}
+	@Override
+	public String getMaHeDaoTao() {
+		// TODO Auto-generated method stub
+		return "ffse";
+	}
+	@Override
+	public String getThongTinHeDaoTao() {
+		// TODO Auto-generated method stub
+		return "Đào tạo dài hạn";
+	}
+	public HeDaoTaoDaiHan(List<MonHoc> dsMonHoc) {
+		super();
+		this.dsMonHoc = dsMonHoc;
+	}
 
-	
 
-	
-	
-
-
->>>>>>> c815716f8d2212d969eef3865790586a577fadc5
 	
 }
