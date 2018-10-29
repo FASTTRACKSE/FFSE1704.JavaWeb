d34fba9b1f6de32d6a5b5cf38d23aac43543d04d
dungkhoa2311 <32055660+dungkhoa2311@users.noreply.github.com>
2018-10-29 17:36:20 +0700

    upcode

---
+++ KHOANN/springcrud/src/com/javatpoint/entity/SinhVien.java
@@ -0,0 +1,40 @@
+package com.javatpoint.entity;
+
+public class SinhVien {
+	private int id;
+	private String name;
+	private int year;
+	private String gender;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
