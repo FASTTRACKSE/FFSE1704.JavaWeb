177a559bb93ee1baaf5d78893a2a1e5a3926f319
NguyenManhhDung <FFSE1702064@st.fasttrack.edu.vn>
2018-10-30 14:51:08 +0700

    update

--- ManhDung/QuanLySinhVien2/src/main/QuanLyHocSinh.java
+++ ManhDung/QuanLySinhVien2/src/main/QuanLyHocSinh.java
@@ -1,29 +1,9 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import giangvien.GiangVienLam;
-import giangvien.GiangVienThang;
-import giangvien.GiangVienThanh;
-import hedaotao.HeDaoTaoDaiHan;
-import hedaotao.HeDaoTaoNganHanJava;
-import hedaotao.HeDaoTaoNganHanPHP;
 import hocsinh.*;
-import lotrinhhoc.LoTrinhHocOffline;
-import lotrinhhoc.LoTrinhHocOnline;
 import monhoc.MonHoc;
-import monhoc.MonHocLapTrinhCoBan;
-import monhoc.MonHocLapTrinhJavaCoBan;
-import monhoc.MonHocLapTrinhJavaWeb;
-import monhoc.MonHocLapTrinhWeb;
-import monhoc.MonHocLapTrinhWebPHP;
-import monhoc.MonHocQuyTrinhDuAn;
-import monhoc.MonHocTinHocVanPhong;
 
 public class QuanLyHocSinh {
 
@@ -31,6 +11,9 @@ public class QuanLyHocSinh {
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("AplicationFile.xml");
 		HocSinh sv1 = context.getBean("sv1", HocSinh.class);
+		HocSinh sv2 = context.getBean("sv2", HocSinh.class);
+		HocSinh sv3 = context.getBean("sv3", HocSinh.class);
+
 
 		System.out.println("Thông tin sinh viên 1: ");
 		System.out.println("   - " + sv1.getThongTinSinhVien());
@@ -38,8 +21,26 @@ public class QuanLyHocSinh {
 		System.out.println("   - " + sv1.getThongTinLoTrinhHoc());
 		for (MonHoc hs : sv1.getHeDaoTao().getDanhsachmonhoc()) {
 			System.out.println("   - " + "Môn học : " + hs.getThongTinMonHoc());
+			System.out.println("   - " + "Giảng viên:" + hs.getTenGiangVien() + " ,Email : " + hs.getEmailGiangVien());
 		}
 		
+		System.out.println("Thông tin sinh viên 2 ");
+		System.out.println("   - " + sv2.getThongTinSinhVien());
+		System.out.println("   - " + sv2.getThongTinHeDaoTao());
+		System.out.println("   - " + sv2.getThongTinLoTrinhHoc());
+		for (MonHoc hs : sv2.getHeDaoTao().getDanhsachmonhoc()) {
+			System.out.println("   - " + "Môn học : " + hs.getThongTinMonHoc());
+			System.out.println("   - " + "Giảng viên:" + hs.getTenGiangVien() + " ,Email : " + hs.getEmailGiangVien());
+		}
+
+		System.out.println("Thông tin sinh viên 3: ");
+		System.out.println("   - " + sv3.getThongTinSinhVien());
+		System.out.println("   - " + sv3.getThongTinHeDaoTao());
+		System.out.println("   - " + sv3.getThongTinLoTrinhHoc());
+		for (MonHoc hs : sv3.getHeDaoTao().getDanhsachmonhoc()) {
+			System.out.println("   - " + "Môn học : " + hs.getThongTinMonHoc());
+			System.out.println("   - " + "Giảng viên:" + hs.getTenGiangVien() + " ,Email : " + hs.getEmailGiangVien());
+		}
 
 		context.close();
 	}
