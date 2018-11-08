ddbc5336c7e3a8ce5e43fff3e79c2642b14faf14
truongdainghia <36950369+truongdainghia@users.noreply.github.com>
2018-11-08 21:58:41 +0700

    update

---
+++ NGHIA TD/SpringWeb/src/ftse1801/demo/spring/model/dao/ISinhVienDAO.java
@@ -0,0 +1,20 @@
+//package ftse1801.demo.spring.model.dao;
+//
+//import java.util.List;
+//
+//import ftse1801.demo.spring.model.bean.students;
+//
+//public interface ISinhVienDAO {
+//
+//	public List<students> getAllSinhVien();
+//
+//	public students getSinhVienByMaSinhVien(final String maSinhVien);
+//
+//	public void updateSinhVien(String maSinhVien, String tenSinhVien, String pass);
+//
+//	public void addSinhVien(String codeSv, String nameSv, String passSv);
+//
+//	public void deleteSinhVien(String maSinhVien);
+//
+//	public List<students> getSinhVienInPage(int currentPage);
+//}
---
+++ NGHIA TD/SpringWeb/src/ftse1801/demo/spring/model/dao/SinhVienDAO2.java
@@ -0,0 +1,50 @@
+//package ftse1801.demo.spring.model.dao;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//
+//import ftse1801.demo.spring.model.bean.students;
+//
+//public class SinhVienDAO2 implements ISinhVienDAO {
+//
+//	@Autowired
+//	private JdbcTemplate template;
+//
+//	@Override
+//	public List<students> getAllSinhVien() {
+//		return null;
+//	}
+//
+//	@Override
+//	public students getSinhVienByMaSinhVien(String maSinhVien) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public void updateSinhVien(String maSinhVien, String tenSinhVien, String pass) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	public void addSinhVien(String codeSv, String nameSv, String passSv) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	public void deleteSinhVien(String maSinhVien) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	public List<students> getSinhVienInPage(int currentPage) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//}
