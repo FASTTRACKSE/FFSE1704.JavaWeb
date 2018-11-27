90c89d1e20b8cd7c3da8463f47cdd83d22a5869f
phanthanhtuan <32055930+phanthanhtuan@users.noreply.github.com>
2018-11-27 23:41:46 +0700

    Create loai_phep_tuanpt.sql

---
+++ Project/database/loai_phep_tuanpt.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2018 at 05:24 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ffse1704`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loai_phep_tuanpt`
+--
+
+CREATE TABLE `loai_phep_tuanpt` (
+  `id_loai_phep` int(11) NOT NULL,
+  `ten_loai_phep` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `loai_phep_tuanpt`
+--
+ALTER TABLE `loai_phep_tuanpt`
+  ADD PRIMARY KEY (`id_loai_phep`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `loai_phep_tuanpt`
+--
+ALTER TABLE `loai_phep_tuanpt`
+  MODIFY `id_loai_phep` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
