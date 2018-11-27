c59a9e6db2aa0762fe43e2953b9a83196412947a
QuangDaiPhanPham <ffse1704002@st.fasttrack.edu.vn>
2018-11-27 23:30:33 +0700

    update database (quan ly du an)

---
+++ Project/database/khach_hang.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2018 at 05:26 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ffse1704`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khach_hang`
+--
+
+CREATE TABLE `khach_hang` (
+  `id` int(11) NOT NULL,
+  `ma_khach_hang` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `ten_khach_hang` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `dia_chi` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `so_dien_thoai` int(11) NOT NULL,
+  `ghi_chu` varchar(500) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `khach_hang`
+--
+
+INSERT INTO `khach_hang` (`id`, `ma_khach_hang`, `ten_khach_hang`, `dia_chi`, `email`, `so_dien_thoai`, `ghi_chu`) VALUES
+(1, 'KH001', 'Công ty CP Việt Hằng', '15 - Phạm Như Xương - Đà Nẵng', '', 0, '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `khach_hang`
+--
+ALTER TABLE `khach_hang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `khach_hang`
+--
+ALTER TABLE `khach_hang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
