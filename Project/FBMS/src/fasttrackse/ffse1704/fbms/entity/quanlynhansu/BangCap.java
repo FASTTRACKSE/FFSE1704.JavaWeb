4144f10aa53f3d89a5f7846aa67a8a6f471c5351
truongdainghia <36950369+truongdainghia@users.noreply.github.com>
2018-11-28 19:57:44 +0700

    update

--- Project/FBMS/src/fasttrackse/ffse1704/fbms/entity/quanlynhansu/BangCap.java
+++ Project/FBMS/src/fasttrackse/ffse1704/fbms/entity/quanlynhansu/BangCap.java
@@ -1,6 +1,7 @@
 package fasttrackse.ffse1704.fbms.entity.quanlynhansu;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="thong_tin_bang_cap")
@@ -25,5 +31,21 @@ public class BangCap implements Serializable{
 	@JoinColumn(name = "ma_nhan_vien", nullable = false)
 	private NhanSu nhanSu;
 	
+	@Column(name = "id_trinh_do", nullable = false)
+	private Integer idTrinhDo;
 	
+	@Column(name = "ten_nganh", nullable = false)
+	private String tenNganh;
+	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Column(name = "thoi_gian", nullable = false)
+	@NotNull
+	private Date thoiGian;
+	
+	@Column(name = "xep_loai", nullable = false)
+	private String xepLoai;
+	
+	@Column(name = "noi_cap", nullable = false)
+	private String noiCap;
 }
