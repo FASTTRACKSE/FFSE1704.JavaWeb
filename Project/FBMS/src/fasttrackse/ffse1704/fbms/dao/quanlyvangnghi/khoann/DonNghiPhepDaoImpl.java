aee2d2dbc8e61844e06991970241536aeae5a45e
dungkhoa2311 <32055660+dungkhoa2311@users.noreply.github.com>
2018-11-29 23:00:51 +0700

    upcode

--- Project/FBMS/src/fasttrackse/ffse1704/fbms/dao/quanlyvangnghi/khoann/DonNghiPhepDaoImpl.java
+++ Project/FBMS/src/fasttrackse/ffse1704/fbms/dao/quanlyvangnghi/khoann/DonNghiPhepDaoImpl.java
@@ -1,5 +1,36 @@
 package fasttrackse.ffse1704.fbms.dao.quanlyvangnghi.khoann;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import fasttrackse.ffse1704.fbms.entity.quanlyvangnghi.khoann.DonNghiPhep;
+@Repository
+
 public class DonNghiPhepDaoImpl implements DonNghiPhepDao {
+
+	
+	
+	@Autowired
+	SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<DonNghiPhep> listDonNghiPhep() {
+		Session session = sessionFactory.getCurrentSession();
+		List<DonNghiPhep> list = session.createQuery("from DonNghiPhep ").list();
+
+		return list;
+	}
 
 }
---
+++ Project/FBMS/src/fasttrackse/ffse1704/fbms/service/quanlyvangnghi/khoann/DonNghiPhepService.java
@@ -0,0 +1,13 @@
+package fasttrackse.ffse1704.fbms.service.quanlyvangnghi.khoann;
+
+import java.util.List;
+
+import fasttrackse.ffse1704.fbms.entity.quanlyvangnghi.khoann.DonNghiPhep;
+
+public interface DonNghiPhepService {
+	
+	
+	public List<DonNghiPhep> listDonNghiPhep();
+	
+	
+}
