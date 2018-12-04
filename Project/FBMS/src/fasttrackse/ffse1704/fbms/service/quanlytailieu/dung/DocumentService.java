c7900aa56551bae9e20a699e0b3a0f81b9f3494b
NguyenManhhDung <FFSE1702064@st.fasttrack.edu.vn>
2018-12-04 22:19:38 +0700

    update

--- Project/FBMS/src/fasttrackse/ffse1704/fbms/service/quanlytailieu/dung/DocumentService.java
+++ Project/FBMS/src/fasttrackse/ffse1704/fbms/service/quanlytailieu/dung/DocumentService.java
@@ -3,6 +3,7 @@ package fasttrackse.ffse1704.fbms.service.quanlytailieu.dung;
 import java.util.List;
 
 import fasttrackse.ffse1704.fbms.entity.quanlytailieu.dung.Document;
+import fasttrackse.ffse1704.fbms.entity.security.PhongBan;
 
 public interface DocumentService  {
 	
@@ -10,5 +11,9 @@ public interface DocumentService  {
 	public List<Document> getAll();
 	
 	public List<Document> getAllPublicDocument();
+	
+	public void saveDraft(Document document);
+	
+	public List<PhongBan> listQuyen();
 }
 	
