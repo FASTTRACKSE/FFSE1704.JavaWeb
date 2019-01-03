78891a1e6ee61eef0e1b34c552d9119d32018b41
nguyensy <32056782+nguyensy@users.noreply.github.com>
2019-01-03 15:25:44 +0700

    Update SyNV_qltl.sql

--- Project/database/SyNV_qltl.sql
+++ Project/database/SyNV_qltl.sql
@@ -71,15 +71,15 @@ CREATE TABLE IF NOT EXISTS `tai_lieu_` (
 --
 
 INSERT INTO `tai_lieu_` (`id`, `ten_tai_lieu`, `ma_danh_muc`, `link_file`, `name_file`, `ma_trang_thai`, `mo_ta`, `ghi_chu`, `ma_phong_ban`, `ma_icon`) VALUES
-(106, 'Nguyễn Mạnh Dũng', 'EL', NULL, '', 'da_phe_duyet', 'English', '', 'PDA1', NULL),
+(106, 'Synv', 'EL', NULL, '', 'da_phe_duyet', 'English', '', 'PDA1', NULL),
 (107, 'Tài liệu Tiếng Anh cơ bản', 'EL', '\\upload\\45628910_563223354173631_2030519606786916352_n.jpg', '45628910_563223354173631_2030519606786916352_n.jpg', 'tu_choi', 'English', NULL, 'PDA3', 'jpg'),
 (127, 'Tài liệu training Nhân Viên mới', 'IT', '\\upload\\31704224_1141763469314929_1979064927918227456_n.jpg', '31704224_1141763469314929_1979064927918227456_n.jpg', 'da_phe_duyet', 'Tài liệu training Nhân Viên mới : cho các thành viên mới vào ', NULL, 'PDA1', 'jpg'),
 (128, 'Tài liệu Tiếng Anh', 'IT', '\\upload\\31704224_1141763469314929_1979064927918227456_n.jpg', '31704224_1141763469314929_1979064927918227456_n.jpg', 'cho_phe_duyet', 'Tài liệu Tiếng Anh: Cho người mới bắt đầu 1', NULL, 'PDA1', 'jpg'),
-(129, 'đạt', 'IT', NULL, '', 'tu_choi', 'đạt', 'Cần bỏ sung thêm chi tiết', 'PDA1', NULL),
+(129, 'alo', 'IT', NULL, '', 'tu_choi', 'đạt', 'Cần bỏ sung thêm chi tiết', 'PDA1', NULL),
 (130, 'Tài Liệu IT', 'IT', '\\upload\\31301842_444188466016062_6080453128756933879_n.jpg', '31301842_444188466016062_6080453128756933879_n.jpg', 'cho_phe_duyet', 'Tài Liệu training ', NULL, 'PDA1', 'jpg'),
 (131, 'Tài liệu Tiếng Anh', 'IT', '\\upload\\29598267_10209043052672843_6711340936042064120_n.jpg', '29598267_10209043052672843_6711340936042064120_n.jpg', 'cho_phe_duyet', 'Hình Ảnh', NULL, 'PDT', 'jpg'),
 (132, 'Tài Liệu bổ sung', 'IT', '\\upload\\Chương trình quản lý thông tin sinh viên Trường FastTrackSE.docx', 'Chương trình quản lý thông tin sinh viên Trường FastTrackSE.docx', 'cho_phe_duyet', 'Bản word', NULL, 'PDA1', 'docx'),
-(133, 'q', 'IT', '\\upload\\31301842_444188466016062_6080453128756933879_n.jpg', '31301842_444188466016062_6080453128756933879_n.jpg', 'cho_phe_duyet', 'a', NULL, 'PDA1', 'jpg');
+(133, 'alooo', 'IT', '\\upload\\31301842_444188466016062_6080453128756933879_n.jpg', '31301842_444188466016062_6080453128756933879_n.jpg', 'cho_phe_duyet', 'a', NULL, 'PDA1', 'jpg');
 
 -- --------------------------------------------------------
 
